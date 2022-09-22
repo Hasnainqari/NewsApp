be8233a700646a9f8372e3ce7a7ca4162cb883cc
Hasnain Qari <qarihasnain43@gmail.com>
2022-09-22 14:55:20 +0500

    logo changed

--- app.js
+++ app.js
@@ -230,3 +230,39 @@ function displayNews() {
     });
 
 }
+
+
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function () {
+      navigator.serviceWorker
+        .register("/serviceWorker.js")
+        .then((res) => console.log("service worker registered"))
+        .catch((err) => console.log("service worker not registered", err));
+    });
+  }
+  
+  function showNotification() {
+    Notification.requestPermission((result) => {
+      if (result === "granted") {
+        navigator.serviceWorker.ready.then((registration) => {
+          registration.showNotification("Saylani Notification", {
+            body: "Notification from #news#",
+            icon: "./logo",
+            vibrate: [200, 100, 200, 100, 200, 100, 200],
+            tag: "vibration-sample",
+          });
+        });
+      }
+    });
+  }
+  
+  showNotification();
+  
+  fetch("https://jsonplaceholder.typicode.com/todos")
+    .then((response) => response.json())
+    .then((json) => {
+      let list = document.getElementById("list");
+      for (var i = 0; i < json.length; i++) {
+        list.innerHTML += `<li>${json[i].title}</li>`;
+      }
+    });
